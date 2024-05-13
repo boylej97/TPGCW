e6247716af8db69e98e218b780dd15feb90da1b8
boylej97 <46059465+boylej97@users.noreply.github.com>
2024-05-13 01:58:39 +0100

    mad update

--- Golf Website/Golf Website/Booking.js
+++ Golf Website/Golf Website/Booking.js
@@ -74,8 +74,17 @@ function toggleStarters() {
     var startersList = document.getElementById('starters-list');
     startersList.style.display = (startersList.style.display === 'none') ? 'block' : 'none';
 }
+function toggleMains() {
+    var mainsList = document.getElementById('mains-list');
+    mainsList.style.display = (mainsList.style.display === 'none') ? 'block' : 'none';
+}
+function toggleDeserts() {
+    var desertsList = document.getElementById('deserts-list');
+    desertsList.style.display = (desertsList.style.display === 'none') ? 'block' : 'none';
+}
 
 function toggleDrinks() {
     var drinksList = document.getElementById('drinks-list');
     drinksList.style.display = (drinksList.style.display === 'none') ? 'block' : 'none';
 }
+
